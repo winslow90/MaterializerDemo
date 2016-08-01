2406d81a90ad604703475a1b80430cc022cdf808
superman90 <winslowleigh@gmail.com>
2016-08-01 14:08:25 -0400

    write 11 card page

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.registry(hub);
 gulp.task('inject', gulp.series('clean',gulp.parallel('styles', 'scripts','fonts','other'), 'inject'));
 gulp.task('serve', gulp.series('inject', 'watch', 'browsersync'));
 gulp.task('default', gulp.series('clean', 'inject'));
-gulp.task('reloadBrowserSync', reloadBrowserSync);
+//gulp.task('reloadBrowserSync', reloadBrowserSync);
 gulp.task('watch', watch);
 
 function reloadBrowserSync(cb) {
@@ -21,11 +21,11 @@ function reloadBrowserSync(cb) {
 
 function watch(done) {
     gulp.watch([
-        conf.path.src('index.html'),
+        conf.path.src('*.html'),
         'bower.json'
     ], gulp.parallel('inject'));
 
-    gulp.watch(conf.path.src('*.html'), gulp.series('inject','reloadBrowserSync'));
+    gulp.watch(conf.path.tmp('*.html'), reloadBrowserSync);
     gulp.watch([
         conf.path.src('**/*.scss'),
         conf.path.src('**/*.css')
