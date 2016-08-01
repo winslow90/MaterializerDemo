09cd87858e829bdafd07c9e3f4514bfbb8cf0376
superman90 <winslowleigh@gmail.com>
2016-08-01 12:40:08 -0400

    wirte 09 btns and fix browsersync conf

--- conf/browsersync.conf.js
+++ conf/browsersync.conf.js
@@ -7,8 +7,7 @@ module.exports = function () {
     return {
         server: {
             baseDir: [
-                conf.paths.tmp,
-                conf.paths.src
+                conf.paths.tmp
             ],
             routes: {
                 '/bower_components': 'bower_components'
