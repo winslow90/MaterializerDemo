d1f1446a25fa78095e635d9a070bb30cc3ed7dd5
superman90 <liwentao90@yahoo.com>
2016-08-02 14:14:53 -0400

    write 26 modals pages

--- conf/browsersync.conf.js
+++ conf/browsersync.conf.js
@@ -13,6 +13,7 @@ module.exports = function () {
                 '/bower_components': 'bower_components'
             }
         },
+        browser: ["google chrome", "firefox"],
         open: false
     };
 };
